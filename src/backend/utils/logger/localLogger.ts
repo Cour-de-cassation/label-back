5d102daf6675a4f434e14b2b9de164e87f3cb0fa
YATERA Boubacar <boubacar.yatera@justice.fr>
2026-04-16 16:58:55 +0200

    refactoring locallogger

--- src/backend/utils/logger/localLogger.ts
+++ src/backend/utils/logger/localLogger.ts
@@ -1,31 +1,15 @@
 export { localLogger };
+import { logger } from '.';
 import { TechLog, DecisionLog } from './loggerType';
 
 const localLogger = {
   info(log: TechLog | DecisionLog) {
-    console.log({
-      level: 'info',
-      operations: log.operations,
-      path: log.path,
-      message: log.message ?? '',
-      decision: 'decision' in log ? { decision: log.decision } : undefined,
-    });
+    logger.info(log);
   },
   warn(log: TechLog) {
-    console.warn({
-      level: 'warn',
-      operations: log.operations,
-      path: log.path,
-      message: log.message ?? '',
-    });
+    logger.warn(log);
   },
   error(log: TechLog & { stack?: string }) {
-    console.error({
-      level: 'error',
-      operations: log.operations,
-      path: log.path,
-      message: log.message ?? '',
-      stack: log.stack ? { stack: log.stack } : undefined,
-    });
+    logger.error(log);
   },
 };
